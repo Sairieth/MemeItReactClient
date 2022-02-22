843ccf73882b7a1946f299bc2756b7e588bfca31
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-02-22 17:07:14 +0100

    feat: imp useEffect to register userInfo from cookie

--- memeit-react-client/src/context/UserProvider.jsx
+++ memeit-react-client/src/context/UserProvider.jsx
@@ -1,16 +1,22 @@
 import { createContext, useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import jwt_decode from 'jwt-decode';
 
 export const userContext = createContext({ user: null, setUser: () => { } });
 
 const UserProvider = ({ children }) => {
-    
-    useEffect(()=>{
-        // read JWT from cookie/local storage
-    },[]);
+    const [cookies] = useCookies(['JWT']);
+    const cookie = cookies.JWT;
+    useEffect(() => {
+        if (cookie!==undefined) {
+            const userInfo = jwt_decode(cookie);
+            setUser(userInfo);
+        }
+    }, [cookie]);
 
     const [user, setUser] = useState(null);
-    
-    return <userContext.Provider value={{user, setUser}}>{children}</userContext.Provider>;
+
+    return <userContext.Provider value={{ user, setUser }}>{children}</userContext.Provider>;
 };
 
 export default UserProvider;
