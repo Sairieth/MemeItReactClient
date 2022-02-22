874fa751d58e155a4f3122d42b3cc7218c56404f
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-02-22 17:13:01 +0100

    feat: cleanup

--- memeit-react-client/src/App.js
+++ memeit-react-client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavigationBarFrame from './components/navigationBars/NavigationBarFrame';
 import { useContext } from 'react';
-import { Button } from 'react-bootstrap';
 import { userContext } from './context/UserProvider';
 import Footer from './components/Footer'
 
@@ -10,20 +9,12 @@ import Footer from './components/Footer'
 
 
 function App() {
-  const { setUser } = useContext(userContext)
-
-  function changeUser() {
-    const userTypes = ['baseUser', 'moderator', 'default'];
-    const userType = userTypes[Math.floor(Math.random() * 3)];
-    console.log(userType);
-    setUser({ role: userType })
-  }
+  const { user } = useContext(userContext);
 
   return (
     <div className="App">
-      <NavigationBarFrame />
+      <NavigationBarFrame user={user} />
       <div className='tempBackground'>
-      <Button onClick={changeUser}>NavBar switch</Button>
       </div>
       <Footer/>
     </div>
