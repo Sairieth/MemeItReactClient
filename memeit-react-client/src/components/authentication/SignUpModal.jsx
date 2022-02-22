b4ff542f3adc0ce06508f69fcc88e555100ac156
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-02-22 17:13:49 +0100

    feat: imp Login/Signup modals

--- memeit-react-client/src/components/authentication/SignUpModal.jsx
+++ memeit-react-client/src/components/authentication/SignUpModal.jsx
@@ -7,18 +7,18 @@ import { useContext } from 'react';
 import jwt_decode from 'jwt-decode';
 
 
-const LoginModal = (props) => {
+const SignUpModal = (props) => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const { data: loginData, request } = usePost('/api/auth/login');
+    const { data: signUpData, request } = usePost('/api/auth/register');
     const { setUser } = useContext(userContext)
     const onHide = props.onHide;
 
     useEffect(() => {
-        if (loginData !== null) {
-            const userInfo = jwt_decode(loginData.token);
+        if (signUpData !== null) {
+            const userInfo = jwt_decode(signUpData.token);
             setUser(userInfo);
         }
-    }, [loginData, setUser, onHide])
+    }, [signUpData, setUser, onHide])
 
     const onSubmit = (input) => {
         request(input);
@@ -34,34 +34,43 @@ const LoginModal = (props) => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    Login
+                    Sign Up
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form id='login-form' onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Group className="mb-3" controlId="loginUsername">
+                <Form id='signUp-form' onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Group className="mb-3" controlId="signUpUsername">
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="text" placeholder="Enter username" {...register('username', { required: true })} />
                         {errors.username && <span>This field is required</span>}
                     </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="loginPassword">
+                    <Form.Group className="mb-3" controlId="signUpPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" {...register('password', { required: true })} />
                         {errors.password && <span>This field is required</span>}
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="loginTimeOffset">
+                    <Form.Group className="mb-3" controlId="signUpEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="text" placeholder="Email" {...register('Email', { required: true })} />
+                        {errors.Email && <span>This field is required</span>}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="signUpDateOfBirth">
+                        <Form.Label>Birthday</Form.Label>
+                        <Form.Control type="date" placeholder="DateOfBirth" {...register('DateOfBirth', { required: true })} />
+                        {errors.DateOfBirth && <span>This field is required</span>}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="signUpTimeOffset">
                         <Form.Control type="hidden" value={Math.abs(new Date().getTimezoneOffset().toFixed())} {...register('offset')} />
                     </Form.Group>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
-                <Button form='login-form' variant="primary" type="submit">
+                <Button form='signUp-form' variant="primary" type="submit">
                     Submit
                 </Button>
             </Modal.Footer>
         </Modal>
-    );
+    )
 }
 
-export default LoginModal
+export default SignUpModal
